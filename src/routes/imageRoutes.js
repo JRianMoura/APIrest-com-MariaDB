c8a59f5690f8d544ad273e38c2f703658000c8f3
judson rian <judsrian@gmail.com>
2024-01-31 11:07:47 -0300

    tratando arquivos que podem ser enviados, para receber apenas png ou jpg

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,13 +1,9 @@
-import multer from 'multer';
 import { Router } from 'express';
 
 import imageController from '../controllers/ImageController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), imageController.store);
+router.post('/', imageController.store);
 
 export default router;
