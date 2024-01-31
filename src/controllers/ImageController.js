8a8cbceda8047eff67fc4fafe0f1f24acc01160a
judson rian <judsrian@gmail.com>
2024-01-31 19:43:26 -0300

    correções para exibir as imagens de cada aluno

--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -1,10 +1,15 @@
+// importando o multer para receber arquivos
 import multer from 'multer';
+// importando as configurações do multer
 import multerConfig from '../config/multerConfig';
 
 import Foto from '../models/Foto';
 
+// variável upload recebendo o multer com suas configurações, e o single serve para receber,
+// apenas um arquivo com o nome 'foto'.
 const upload = multer(multerConfig).single('foto');
 
+// crição da classe
 class ImageController {
   store(req, res) {
     return upload(req, res, async (err) => {
@@ -13,14 +18,25 @@ class ImageController {
           errors: [err.code],
         });
       }
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        const foto = await Foto.create({ originalname, filename, aluno_id });
 
-      const { originalname, filename } = req.file;
-      const { aluno_id } = req.body;
-      const foto = await Foto.create({ originalname, filename, aluno_id });
-
-      return res.json(foto);
+        return res.json(foto);
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
     });
   }
 }
 
 export default new ImageController();
+
+// Index - Lista os dados da tabela
+// Show - Exibe um item especifico
+// Update - Salva a atualização do dado
+// Store - Salva o novo item na tabela
+// Delete - Remove o dado
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -10,7 +10,7 @@ const router = new Router();
 // router.get('/:id', userController.show);
 
 // Usado em sistema Real
-router.post('/', userController.store);
+router.post('/', loginRequired, userController.store);
 router.put('/', loginRequired, userController.update);
 router.delete('/', loginRequired, userController.delete);
 
